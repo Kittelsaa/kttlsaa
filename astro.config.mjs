d04d6505ab0d8f844395454d02b7b005cee905bf
Nyampundu <113338566+Kittelsaa@users.noreply.github.com>
2025-06-30 01:21:37 +0200

    updates-newsletter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,22 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kttlsaa.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
+
+  // The experimental flag has been removed as it's no longer needed in Astro 5
   markdown: {
     shikiConfig: {
       theme: 'github-light',
       wrap: true
     }
-  }
-  // The experimental flag has been removed as it's no longer needed in Astro 5
+  },
+
+  adapter: vercel()
 });
-
-
